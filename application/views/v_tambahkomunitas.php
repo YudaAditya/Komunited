9e7581d902ab20e746db0472bd9a564ae358a5de
Unknown <israfarliadi14@gmail.com>
2018-06-20 22:36:42 +0700

    ...

--- application/views/v_tambahkomunitas.php
+++ application/views/v_tambahkomunitas.php
@@ -118,7 +118,7 @@
 							</button>
 						</div>
 
-						<a href="<?php echo base_url('index.php'); ?>" class="dis-block txt3 hov1 p-r-30 p-t-10 p-b-10 p-l-30">
+						<a href="<?php echo base_url('index.php/Anggota'); ?>" class="dis-block txt3 hov1 p-r-30 p-t-10 p-b-10 p-l-30">
 							Beranda
 							<i class="fa fa-long-arrow-right m-l-5"></i>
 						</a>
